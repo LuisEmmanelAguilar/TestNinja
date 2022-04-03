43c290280324261a8a35a65205eeed499efcc98d
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-04-03 01:06:26 -0600

    -Testing Arrays and Collections

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Math = TestNinja.Fundamentals.Math;
 
@@ -44,6 +45,32 @@ namespace TestNinja.UnitTests
             Assert.That(result, Is.EqualTo(expectedResult));
         }
 
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
+        {
+            var result = _math.GetOddNumbers(5);
+
+            // A little bit too general...always will pass
+            Assert.That(result, Is.Not.Empty);
+
+            // A little bit more specific
+            Assert.That(result.Count(), Is.EqualTo(3));
+
+            // A litlle bit more general...
+            Assert.That(result, Does.Contain(1));
+            Assert.That(result, Does.Contain(3));
+            Assert.That(result, Does.Contain(5));
+
+            // Cleaner way to write the last 3 Assertions
+            Assert.That(result, Is.EquivalentTo(new[] { 1, 3, 5 }));
+
+            // Make you sure that is ordered
+            Assert.That(result, Is.Ordered);
+
+            // Make you sure that there are not duplicate items in the collection
+            Assert.That(result, Is.Unique);
+        }
 
     }
 }
