6b804f1f4ea832f42e4acf8279b6b4666d38a1f3
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-04-10 18:23:11 -0500

    Excercise Using Moq

---
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,50 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> _mockFileDownloader;
+        private InstallerHelper _installerHelper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockFileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_mockFileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnFalse()
+        {
+            // Arrange
+            _mockFileDownloader.Setup(fd => 
+                fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<WebException>();
+
+            // Act
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadCompletes_ReturnTrue()
+        {
+            // Act
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+
+    }
+}
---
+++ TestNinja/Mocking/FileDownloader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking
+{
+    public interface IFileDownloader
+    {
+        void DownloadFile(string url, string path);
+    }
+
+    public class FileDownloader : IFileDownloader
+    {
+        public void DownloadFile(string url, string path)
+        {
+            // Encaptulating the code that touches an external resource inside the FileDownloader
+            var client = new WebClient();
+            client.DownloadFile(url, path);
+        }
+    }
+}
