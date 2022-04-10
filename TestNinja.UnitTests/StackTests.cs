eba7a65677dbb1d7acfb570db3a68c7341ae793c
Luis Emmanuel Aguilar <luis.emmanuel.aguilar@gmail.com>
2022-04-09 23:53:03 -0500

    Excercise Stack Tests

---
+++ TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,118 @@
+﻿using NUnit.Framework;
+using System;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void Push_ArgumentIsNlull_ThrowArgNullException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_ValidArg_AddTheObjectToTheStack()
+        {
+            var stack = new Stack<string>();
+
+            stack.Push("a");
+
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowInvalidOperationException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObjects_ReturnObjectOnTop()
+        {
+            // Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            // Act
+            var result = stack.Pop();
+
+            //Assert
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObjects_RemoveObjectOnTop()
+        {
+            //Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            // Act
+            stack.Pop();
+
+            //Assert
+            Assert.That(stack.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Peek_EmptyStack_ThrowInvalidOperationException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_ReturnObjectTopOfTheStack()
+        {
+            //Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            // Act
+            var result = stack.Peek();
+
+            //Assert
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_DoesNotRemoveObjectOnTopOfTheStack()
+        {
+            //Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            // Act
+            stack.Peek();
+
+            //Assert
+            Assert.That(stack.Count, Is.EqualTo(3));
+        }
+
+    }
+}
--- TestNinja/Fundamentals/Stack.cs
+++ TestNinja/Fundamentals/Stack.cs
@@ -7,16 +7,19 @@ namespace TestNinja.Fundamentals
     {
         private readonly List<T> _list = new List<T>();
 
+        // returns the count on the stack
         public int Count => _list.Count;
 
+        // adds an object on top of the stack
         public void Push(T obj)
         {
             if (obj == null)
                 throw new ArgumentNullException();
-            
+
             _list.Add(obj);
         }
 
+        // removes the object on top of the stack and also returns it
         public T Pop()
         {
             if (_list.Count == 0)
@@ -25,10 +28,10 @@ namespace TestNinja.Fundamentals
             var result = _list[_list.Count - 1];
             _list.RemoveAt(_list.Count - 1);
 
-            return result; 
+            return result;
         }
 
-
+        // returns the object at top of the stack
         public T Peek()
         {
             if (_list.Count == 0)
